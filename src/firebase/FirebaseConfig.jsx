523db5af147b08a8116373ca9498b062ba6f4c57
Devraj Songara <118315477+devraj894@users.noreply.github.com>
2024-07-09 18:45:46 +0000

    Updated for a security....

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -5,12 +5,12 @@ import {getFirestore} from 'firebase/firestore'
 import {getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGP1IL5Ra5vwISFnkit5_3CBmaC4pTIaA",
-  authDomain: "ecommerce-bb3a4.firebaseapp.com",
-  projectId: "ecommerce-bb3a4",
-  storageBucket: "ecommerce-bb3a4.appspot.com",
-  messagingSenderId: "362312925478",
-  appId: "1:362312925478:web:e35a7feafe60ebab70c1e7"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
